0ee684b1c7e9a8ca13c8b87e324d3119219e4845
Illya Sokolov <illya.sokolov82@gmail.com>
2021-03-23 11:59:50 +0200

    Angular Bootstrap, product-list component, product entity

---
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+// ng generate component components/product-list
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
