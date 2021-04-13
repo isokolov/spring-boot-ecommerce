9069de5d51e9de52f0d56b4af59ce2999df0427d
Illya Sokolov <illya.sokolov82@gmail.com>
2021-04-13 17:23:34 +0300

    Update Backend & Frontend to display products by category

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,12 +9,17 @@ import {map} from "rxjs/operators";
 })
 export class ProductService {
 
-  private baseUrl = 'http://localhost:8080/api/products?size=100';
+  private baseUrl = 'http://localhost:8080/api/products';
 
   constructor(private httpClient:HttpClient) { }
 
-  getProductList(currentCategoryId: number): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]> {
+
+    // need to build URL based on category id
+    // http://localhost:8080/api/products/search/findByCategoryId?id=
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
