6df506b19759b5537c29442b77d6ee804064da9b
Illya Sokolov <illya.sokolov82@gmail.com>
2021-04-18 18:01:51 +0300

    Backend and frontend for search component

---
+++ frontend/angular-ecommerce/src/app/components/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+// ng generate component components/search
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  doSearch(value: string) {
+    console.log(`value={${value}}`);
+  }
+}
--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    //  http://localhost:8080/api/products/search/findByCategoryId?id={your input}
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    //  http://localhost:8080/api/products/search/findByNameContaining?name={your input}
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 
 }
