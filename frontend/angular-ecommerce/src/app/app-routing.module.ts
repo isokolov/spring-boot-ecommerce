c4a13ce4d2b7a9a20fe1c2d95789d21ca54b2af9
Illya Sokolov <illya.sokolov82@gmail.com>
2021-05-03 14:27:36 +0300

    cart details component, retrieve data with cart service

--- frontend/angular-ecommerce/src/app/app-routing.module.ts
+++ frontend/angular-ecommerce/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProductListComponent} from "./components/product-list/product-list.component";
 import {ProductDetailsComponent} from "./components/product-details/product-details.component";
+import {CartDetailsComponent} from "./components/cart-details/cart-details.component";
 
 const routes: Routes = [
+  {path: 'cart-details', component: CartDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'category/:id', component: ProductListComponent},
