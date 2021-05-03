4a5b35feb8fb9364ccbcd38160d2ae77db0d0a7e
Illya Sokolov <illya.sokolov82@gmail.com>
2021-05-03 15:43:57 +0300

    checkout component, reactive forms

--- frontend/angular-ecommerce/src/app/app-routing.module.ts
+++ frontend/angular-ecommerce/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {ProductListComponent} from "./components/product-list/product-list.component";
 import {ProductDetailsComponent} from "./components/product-details/product-details.component";
 import {CartDetailsComponent} from "./components/cart-details/cart-details.component";
+import {CheckoutComponent} from "./components/checkout/checkout.component";
 
 const routes: Routes = [
+  {path: 'checkout', component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
--- frontend/angular-ecommerce/src/app/app.module.ts
+++ frontend/angular-ecommerce/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -26,13 +28,15 @@ import { CartDetailsComponent } from './components/cart-details/cart-details.com
     SearchComponent,
     ProductDetailsComponent,
     CartStatusComponent,
-    CartDetailsComponent
+    CartDetailsComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
